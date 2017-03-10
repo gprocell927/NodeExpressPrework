ac6613ce56cc0525e22fe50a17f2c49918ed2e97
Gabi Procell <gprocell927@gmail.com>
2017-03-10 13:09:35 -0700

    form passes two values using the POST method

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
 var express = require ('express')
 var app = express()
+var bodyParser = require('body-parser')
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 app.use(express.static('public'))
-
 app.get('/index.htm', function (req, res) {
   res.sendFile( __dirname + '/' + 'index.htm')
 })
 
-app.get('/process_get', function (req, res) {
+app.post('/process_post', urlencodedParser, function (req, res) {
   response = {
-    first_name: req.query.first_name,
-    last_name: req.query.last_name
+    first_name: req.body.first_name,
+    last_name: req.body.last_name
   }
   console.log(response)
   res.end(JSON.stringify(response))
